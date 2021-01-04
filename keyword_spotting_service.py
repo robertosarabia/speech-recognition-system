0b31e0b367c563f924ba424256f76104dcbb14ce
robertosarabia <sarabia.fx@gmail.com>
2021-01-03 18:18:25 -0800

    pre-test version of keyword spotting service added. TODO note left to record and add audio test files

--- keyword_spotting_service.py
+++ keyword_spotting_service.py
@@ -1,7 +1,10 @@
 import tensorflow.keras as keras
+import numpy as np
+import librosa
 
 
 MODEL_PATH = "model.h5"
+NUM_SAMPLES_TO_CONSIDER = 22050 # 1 sec of sound
 
 class _Keyword_Spotting_Service:
 
@@ -25,11 +28,33 @@ class _Keyword_Spotting_Service:
     def predict(selfself, file_path):
 
         # extract MFCCs
+        MFCCs = self.prepreprocess(file_path) #
 
         # convert 2d MFCCs array into 4d array
+        MFCCs = MFCCs[np.newaxis, ..., np.newaxis]
 
         # make prediction
+        predictions = self.model.predict(MFCCs) # [ [] ]
+        predicted_index = np.argmax(predictions)
+        predicted_keyword = self._mappings[predicted_index]
 
+        return predicted_keyword
+
+
+    def preprocess(self, file_path, n_mfcc=13, n_fft=2048, hop_length=512):
+
+        # load audio file
+        signal, sr = librosa.load(file_path)
+
+        # ensure consistency in the audio file length
+        if len(signal) > NUM_SAMPLES_TO_CONSIDER:
+            signal = signal[:NUM_SAMPLES_TO_CONSIDER]
+
+        # extract MFCCs
+        MFCCs = librosa.feature.mfcc(signal, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+
+        # transpose the returned MFCCs
+        return MFCCs.T
 
 # elegant way of implementing singleton class in Python
 # checks if instance is available and loads otherwise creates new using MODEL_PATH
@@ -40,3 +65,14 @@ def Keyword_Spotting_Service():
         _Keyword_Spotting_Service._instance = _Keyword_Spotting_Service
         Keyword_Spotting_Service.model = keras.models.load_model(MODEL_PATH)
     return _Keyword_Spotting_Service._instance
+
+
+if __name__ = "__main__":
+
+    kss = Keyword_Spotting_Service()
+
+    # TODO create file structure with below audio test files
+    kss.predict("test/down.wav")
+    kss.predict("test/left.wav")
+
+    print(f"Predicted Keywords: {keyword1}, {keyword2}")
